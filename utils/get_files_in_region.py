47c13bc0a265972381878cd15e48bc4a9d675c94
JP Swinski <jp.swinski@nasa.gov>
2021-04-28 15:00:18 -0400

    added utility for returning names of files in a region

---
+++ utils/get_files_in_region.py
@@ -0,0 +1,37 @@
+#
+# Imports
+#
+import sys
+import json
+import geopandas
+from sliderule import icesat2
+
+###############################################################################
+# MAIN
+###############################################################################
+
+if __name__ == '__main__':
+
+    # Set Filename
+    filename = sys.argv[1]
+
+    # Override Tolerance
+    tolerance = 0.0
+    if len(sys.argv) > 2:
+        tolerance = float(sys.argv[2])
+
+    # Override dataset
+    dataset='ATL03'
+    if len(sys.argv) > 3:
+        dataset = sys.argv[3]
+
+    # Override region of interest
+    regions = icesat2.toregion(filename, tolerance)
+
+    # Query CMR for list of resources
+    for i in range(len(regions)):
+        region = regions[i]
+        resources = icesat2.cmr(region, short_name=dataset)
+        print("\nRegion {}: {} points, {} files".format(i, len(region), len(resources)))
+        for resource in resources:
+            print(resource)
