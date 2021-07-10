38f86b88ea58b678df511b81934d9d2aacc07547
JP Swinski <jp.swinski@nasa.gov>
2021-07-09 22:44:37 -0400

    wrapped error handling for servers in a try/except block to handle when a server has been removed from table and then an existing thread errors out

--- sliderule/sliderule.py
+++ sliderule/sliderule.py
@@ -1,30 +1,30 @@
 # Copyright (c) 2021, University of Washington
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice, 
+#
+# 1. Redistributions of source code must retain the above copyright notice,
 #    this list of conditions and the following disclaimer.
 #
-# 2. Redistributions in binary form must reproduce the above copyright notice, 
-#    this list of conditions and the following disclaimer in the documentation 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-# 
-# 3. Neither the name of the University of Washington nor the names of its 
-#    contributors may be used to endorse or promote products derived from this 
+#
+# 3. Neither the name of the University of Washington nor the names of its
+#    contributors may be used to endorse or promote products derived from this
 #    software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY OF WASHINGTON AND CONTRIBUTORS
-# “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
+# “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 # TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF WASHINGTON OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import requests
@@ -53,7 +53,7 @@ logger = logging.getLogger(__name__)
 recdef_lock = threading.Lock()
 recdef_table = {}
 
-gps_epoch = datetime(1980, 1, 6) 
+gps_epoch = datetime(1980, 1, 6)
 tai_epoch = datetime(1970, 1, 1, 0, 0, 10)
 
 eventformats = {
@@ -163,10 +163,13 @@ def __getserv():
 def __errserv(serv):
     global server_table, server_max_errors
     with server_lock:
-        server_table[serv] += 1
-        logger.critical(serv + " encountered consecutive error " + str(server_table[serv]))
-        if server_table[serv] > server_max_errors:
-            server_table.pop(serv, None)
+        try:
+            server_table[serv] += 1
+            logger.critical(serv + " encountered consecutive error " + str(server_table[serv]))
+            if server_table[serv] > server_max_errors:
+                server_table.pop(serv, None)
+        except Exception as e:
+            logger.critical(serv + " already removed from table")
 
 #
 #  __clrserv
@@ -410,9 +413,9 @@ def set_max_errors (max_errors):
 # GPS2UTC
 #
 def gps2utc (gps_time, as_str=True):
-    gps_time = gps_epoch + timedelta(seconds=gps_time) 
+    gps_time = gps_epoch + timedelta(seconds=gps_time)
     tai_time = gps_time + timedelta(seconds=19)
-    tai_timestamp = (tai_time - tai_epoch).total_seconds() 
+    tai_timestamp = (tai_time - tai_epoch).total_seconds()
     utc_timestamp = datetime.utcfromtimestamp(tai_timestamp)
     if as_str:
         return str(utc_timestamp)
