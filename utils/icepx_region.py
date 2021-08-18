f3d777427af90c2aa64d3b3f43ca39eea31a52ab
JP Swinski <jp.swinski@nasa.gov>
2021-08-18 12:07:51 -0400

    added plot to icepx_region script

--- utils/icepx_region.py
+++ utils/icepx_region.py
@@ -2,6 +2,7 @@ import sys
 from datetime import date
 from sliderule import ipxapi
 from sliderule import icesat2
+import matplotlib.pyplot as plt
 import icepyx
 
 ###############################################################################
@@ -15,7 +16,7 @@ def parse_command_line(args, cfg):
             if args[i] == '--'+entry:
                 if type(cfg[entry]) is str:
                     cfg[entry] = args[i + 1]
-                elif type(cfg[entry]) is list:
+                elif (type(cfg[entry]) is list) or (cfg[entry] is None):
                     l = []
                     while (i + 1) < len(args) and args[i + 1].isnumeric():
                         l.append(int(args[i + 1]))
@@ -37,7 +38,7 @@ if __name__ == '__main__':
 
     # set script defaults
     scfg = {
-        "url": ['127.0.0.1'],
+        "url": '127.0.0.1',
         "asset": 'atlas-local'
     }
 
@@ -46,8 +47,8 @@ if __name__ == '__main__':
         "short_name": 'ATL03',
         "spatial_extent": 'valgrande.shp',
         "date_range": ['2018-01-01', "{}-{}-{}".format(today.year, today.month, today.day)],
-        "cycles": [],
-        "tracks": []
+        "cycles": None,
+        "tracks": None
     }
 
     # set processing parameter defaults
@@ -66,27 +67,30 @@ if __name__ == '__main__':
     parse_command_line(sys.argv, scfg)
     parse_command_line(sys.argv, parms)
 
-    # massage command line parameters
-    if len(icfg["cycles"]) == 0:
-        icfg["cycles"] = None
-    if len(icfg["tracks"]) == 0:
-        icfg["tracks"] = None
+    # bypass service discovery if url is localhost
+    if scfg["url"] == '127.0.0.1':
+        scfg["url"] = ['127.0.0.1']
 
     # create icepx region
     iregion = icepyx.Query(icfg["short_name"], icfg["spatial_extent"], icfg["date_range"], cycles=icfg["cycles"], tracks=icfg["tracks"])
 
     # visualize icepx region
-    iregion.visualize_spatial_extent()
+    # iregion.visualize_spatial_extent()
 
     # display summary information
     iregion.product_summary_info()
-    print("Available Granules:", iregion.avail_granules())
-    print("Available Granule IDs:", iregion.avail_granules(ids=True))
+    # print("Available Granules:", iregion.avail_granules())
+    # print("Available Granule IDs:", iregion.avail_granules(ids=True))
 
     # initialize sliderule api
     icesat2.init(scfg["url"], verbose=True)
 
     # generate sliderule atl06 elevations
-    parms["poly"] = icesat2.toregion(icfg["spatial_extent"])
-    ipxapi.atl06p(iregion, parms, scfg["asset"])
+    parms["poly"] = icesat2.toregion(icfg["spatial_extent"])[0]
+    atl06_sr = ipxapi.atl06p(iregion, parms, scfg["asset"])
 
+    # Create Plot
+    f, ax = plt.subplots()
+    vmin, vmax = atl06_sr['h_mean'].quantile((0.02, 0.98))
+    atl06_sr.plot(ax=ax, column='h_mean', cmap='inferno', s=0.1, vmin=vmin, vmax=vmax)
+    plt.show()
