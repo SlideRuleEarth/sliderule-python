600d5bc4b5f38b23f736e6da57c53f6920c801f6
JP Swinski <jp.swinski@nasa.gov>
2022-04-27 13:53:35 -0400

    fixed cmr pytest

--- tests/test_icesat2.py
+++ tests/test_icesat2.py
@@ -53,4 +53,4 @@ class TestRemote:
             time_start='2018-10-01',
             time_end='2018-12-01')
         assert isinstance(granules, list)
-        assert 'ATL03_20181017222812_02950102_004_01.h5' in granules
+        assert 'ATL03_20181017222812_02950102_005_01.h5' in granules
