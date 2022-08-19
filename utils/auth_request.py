7b0c2162b2905fa25164838e8bf86d1efe954c7f
JP Swinski <jp.swinski@nasa.gov>
2022-08-19 08:52:51 -0400

    parameterized auth request

--- utils/auth_request.py
+++ utils/auth_request.py
@@ -2,6 +2,7 @@
 # Perform a authentication request
 #
 
+import sys
 import json
 import requests
 
@@ -11,11 +12,16 @@ import requests
 
 if __name__ == '__main__':
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    organization = sys.argv[3]
+
     # Authentication Request
     url = "https://ps.testsliderule.org/ps/api/org_token/"
-    rqst = {"username": "<username>", "password": "<password>", "org_name": "<organization>"}
+    rqst = {"username": username, "password": password, "org_name": organization}
     headers = {'Content-Type': 'application/json'}
     rsps = requests.post(url, data=json.dumps(rqst), headers=headers, timeout=(60,10)).json()
+    print("PS Response: ", rsps)
     refresh = rsps["refresh"]
     access = rsps["access"]
     print("My Token: ", access)
