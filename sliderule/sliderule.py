a9639d797cb721c55a82c14f79bdfeb645b42f37
JP Swinski <jp.swinski@nasa.gov>
2022-11-02 16:00:51 +0000

    demoted missing netrc file to a warning

--- sliderule/sliderule.py
+++ sliderule/sliderule.py
@@ -637,7 +637,7 @@ def authenticate (ps_organization, ps_username=None, ps_password=None):
             ps_username = login_credentials[0]
             ps_password = login_credentials[2]
         except Exception as e:
-            logger.error("Failed to retrieve username and password from netrc file: {}".format(e))
+            logger.warning("Failed to retrieve username and password from netrc file: {}".format(e))
 
     # authenticate to provisioning system
     if ps_username and ps_password:
