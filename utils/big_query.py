85b31f2de54d52ef0859d264dfb5fa976bc4de7b
JP Swinski <jp.swinski@nasa.gov>
2022-11-17 17:21:41 +0000

    continued work on big_query test

--- utils/big_query.py
+++ utils/big_query.py
@@ -1,5 +1,15 @@
 from sliderule import icesat2
-icesat2.init("localhost", True, organization=None)
+icesat2.init("slideruleearth.io", True, organization="sliderule")
+
+rec_cnt = 0
+ph_cnt = 0
+
+def atl03rec_cb(rec):
+  global rec_cnt, ph_cnt
+  rec_cnt += 1
+  ph_cnt += rec["count"][0] + rec["count"][1]
+  print("{} {}".format(rec_cnt, ph_cnt), end='\r')
+
 params={'srt': 1,
  'len': 20,
  'track': 0,
@@ -12,4 +22,5 @@ params={'srt': 1,
   {'lat': -76.5, 'lon': 59.6},
   {'lat': -76.5, 'lon': 67.7},
   {'lat': -64.5, 'lon': 67.7}]}
-gdf = icesat2.atl03s(params, asset="nsidc-s3",  resource='ATL03_20190502021224_05160312_005_01.h5')
+
+gdf = icesat2.atl03sp(params, asset="nsidc-s3", resources=['ATL03_20190502021224_05160312_005_01.h5'], callbacks = {"atl03rec": atl03rec_cb})
