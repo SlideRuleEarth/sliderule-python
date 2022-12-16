1cdaf142c467200a97da84cbcf94c100933da58f
JP Swinski <jp.swinski@nasa.gov>
2022-12-16 15:16:47 +0000

    added initial sampler test

--- tests/test_arcticdem.py
+++ tests/test_arcticdem.py
@@ -1,8 +1,12 @@
 """Tests for sliderule-python arcticdem raster support."""
 
 import pytest
+from pathlib import Path
+import os.path
 import sliderule
 from sliderule import icesat2
+
+TESTDIR = Path(__file__).parent
 
 @pytest.mark.network
 class TestVrt:
@@ -12,3 +16,17 @@ class TestVrt:
         rsps = sliderule.source("samples", rqst)
         assert abs(rsps["samples"][0][0]["value"] - 80.713500976562) < 0.001
         assert rsps["samples"][0][0]["file"] == '/vsis3/pgc-opendata-dems/arcticdem/mosaics/v3.0/2m/70_09/70_09_2_1_2m_v3.0_reg_dem.tif'
+
+    def test_sampler(self, domain, asset, organization):
+        icesat2.init(domain, organization=organization)
+        resource = "ATL03_20190314093716_11600203_005_01.h5"
+        region = icesat2.toregion(os.path.join(TESTDIR, "data/dicksonfjord.geojson"))
+        parms = { "poly": region['poly'],
+                  "cnf": "atl03_high",
+                  "ats": 20.0,
+                  "cnt": 10,
+                  "len": 40.0,
+                  "res": 20.0,
+                  "maxi": 1,
+                  "samples": ["arcticdem-mosaic"] }
+        gdf = icesat2.atl06p(parms, asset=asset, resources=[resource])
