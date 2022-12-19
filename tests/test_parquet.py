6e16bfdd1ce7fd0769b4d758aeb461cb9164c40d
JP Swinski <jp.swinski@nasa.gov>
2022-12-19 14:14:21 +0000

    added parquet pytest

--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -2,22 +2,15 @@
 
 import pytest
 from pathlib import Path
+import os
 import os.path
-import sliderule
 from sliderule import icesat2
 
 TESTDIR = Path(__file__).parent
 
 @pytest.mark.network
-class TestVrt:
-    def test_vrt(self, domain, organization):
-        icesat2.init(domain, organization=organization)
-        rqst = {"dem-asset": "arcticdem-mosaic", "coordinates": [[-178.0,51.7]]}
-        rsps = sliderule.source("samples", rqst)
-        assert abs(rsps["samples"][0][0]["value"] - 80.713500976562) < 0.001
-        assert rsps["samples"][0][0]["file"] == '/vsis3/pgc-opendata-dems/arcticdem/mosaics/v3.0/2m/70_09/70_09_2_1_2m_v3.0_reg_dem.tif'
-
-    def test_sampler(self, domain, asset, organization):
+class TestParquet:
+    def test_parquet(self, domain, asset, organization):
         icesat2.init(domain, organization=organization)
         resource = "ATL03_20190314093716_11600203_005_01.h5"
         region = icesat2.toregion(os.path.join(TESTDIR, "data/dicksonfjord.geojson"))
@@ -29,5 +22,13 @@ class TestVrt:
                   "len": 40.0,
                   "res": 20.0,
                   "maxi": 1,
-                  "samples": ["arcticdem-mosaic"] }
+                  "output": { "path": "testfile.parquet", "format": "parquet", "open_on_complete": True } }
         gdf = icesat2.atl06p(parms, asset=asset, resources=[resource])
+        assert len(gdf) == 256
+        assert len(gdf.keys()) == 17
+        assert gdf["rgt"][0] == 1160
+        assert gdf["cycle"][0] == 2
+        assert gdf["segment_id"][0] >= 405240
+        assert gdf["segment_id"][255] <= 405609
+        os.remove("testfile.parquet")
+
