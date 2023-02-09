e9123113f1c4dd1a7ac15e5db9a58b895eec3172
JP Swinski <jp.swinski@nasa.gov>
2023-02-09 01:28:43 +0000

    updated arcticdem test to latest server code

--- tests/test_arcticdem.py
+++ tests/test_arcticdem.py
@@ -12,7 +12,7 @@ TESTDIR = Path(__file__).parent
 class TestVrt:
     def test_vrt(self, domain, organization):
         icesat2.init(domain, organization=organization)
-        rqst = {"dem-asset": "arcticdem-mosaic", "coordinates": [[-178.0,51.7]]}
+        rqst = {"samples": {"asset": "arcticdem-mosaic"}, "coordinates": [[-178.0,51.7]]}
         rsps = sliderule.source("samples", rqst)
         assert abs(rsps["samples"][0][0]["value"] - 80.713500976562) < 0.001
         assert rsps["samples"][0][0]["file"] == 'pgc-opendata-dems/arcticdem/mosaics/v3.0/2m/70_09/70_09_2_1_2m_v3.0_reg_dem.tif'
