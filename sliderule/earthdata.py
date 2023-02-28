34345b73c698a34f19111ebd9201d0cd359eec39
JP Swinski <jp.swinski@nasa.gov>
2023-02-28 22:24:41 +0000

    fixed broken link in earthdata module

--- sliderule/earthdata.py
+++ sliderule/earthdata.py
@@ -260,7 +260,7 @@ def cmr(provider=None, short_name=None, version=None, polygon=None, time_start='
     Parameters
     ----------
         polygon:    list
-                    either a single list of longitude,latitude in counter-clockwise order with first and last point matching, defining region of interest (see `polygons </rtd/user_guide/ICESat-2.html#id1>`_), or a list of such lists when the region includes more than one polygon
+                    either a single list of longitude,latitude in counter-clockwise order with first and last point matching, defining region of interest (see `polygons </rtd/user_guide/SlideRule.html#polygons>`_), or a list of such lists when the region includes more than one polygon
         time_start: str
                     starting time for query in format ``<year>-<month>-<day>T<hour>:<minute>:<second>Z``
         time_end:   str
